e8689618a479289620f4e899e37e1443b911348e
Henrique Naimaier <coubron@yahoo.com.br>
2021-01-15 09:33:58 -0300

    Creating components

--- spa_js/src/componentes/lista/listagem-cliente.js
+++ spa_js/src/componentes/lista/listagem-cliente.js
@@ -2,6 +2,7 @@ import {
   deletaCliente,
   listarClientes
 } from '../../api/cliente.js'
+import inicializaCadastro from '../cadastro/componente-cadastro.js'
 
 import '../../assets/css/clientes.css'
 
@@ -13,7 +14,30 @@ const removeCliente = (id) => {
   }
 }
 
-const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+const conteudo = 
+`<thead class="thead-dark">
+  <tr>
+    <th scope="col">CPF</th>
+    <th scope="col">Nome</th>
+    <th scope="col"></th>
+    <th scope="col"><a class="btn btn-primary">Novo Cliente</a></th>
+  </tr>
+</thead>`
+
+const container = document.querySelector('[data-container]')
+const tabela = document.createElement('table')
+
+tabela.innerHTML = conteudo
+tabela.classList.add('table')
+container.appendChild(tabela)
+
+const novoCliente = document.querySelector('.btn')
+
+novoCliente.addEventListener('click', () => {
+  inicializaCadastro()
+})
+
+const corpoTabela = document.createElement('tbody')
 
 const exibeCliente = (cpf, nome, id) => {
     const linha = document.createElement('tr');
@@ -24,10 +48,7 @@ const exibeCliente = (cpf, nome, id) => {
     <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     <a href="./componentes/edita/edita-clientes.html?id=${id}">
     <button type=""button class="btn btn-info">Editar</button>
-    </a>
-    
-    
-`
+    </a>`
   
     linha.innerHTML = conteudoLinha;
     return linha;
@@ -37,9 +58,9 @@ const exibeCliente = (cpf, nome, id) => {
   exibe.forEach(indice => {
     corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
   })
- }
+ })
 
- )
+ tabela.appendChild(corpoTabela)
   
   
  
