7809cfe63deef65e98afc39110ed5e0d729a24c4
Henrique Naimaier <coubron@yahoo.com.br>
2021-03-02 17:10:37 -0300

    First commit

---
+++ react-como-funciona/ceep/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva a sua nota..." />
+        <button>Criar Nota</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
